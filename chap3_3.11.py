c289cb573dd2849b6904d85e20e7e7ed9129b41f
Lsobaca <61958964+Lsobaca@users.noreply.github.com>
2022-04-16 15:18:45 -0700

    answered all the questions in chap3

--- chap3_3.11.py
+++ chap3_3.11.py
@@ -3,8 +3,14 @@
 # Enter Hours: 45
 # Enter Rate: 10
 # Pay: 475.0
-
-
+hours = int(input("Enter Hours: "))
+rate  = int(input("Enter Rate: "))
+if hours > 40:
+    overtime = (hours - 40)
+    overtimerate = rate * 1.5
+    print(f"Pay: {40 * rate + (overtime * overtimerate)}" )
+else:
+    print(f'Pay: {rate * hours}')
 
 # Exercise 2: Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the
 # program. The following shows two executions of the program:
@@ -13,6 +19,17 @@
 # Error, please enter numeric input
 # Enter Hours: forty
 # Error, please enter numeric input
+try:
+    hours = int(input("Enter Hours: "))
+    rate  = int(input("Enter Rate: "))
+    if hours > 40:
+        overtime = (hours - 40)
+        overtimerate = rate * 1.5
+        print(f"Pay: {40 * rate + (overtime * overtimerate)}" )
+    else:
+        print(f'Pay: {rate * hours}')
+except:
+    print("Enter a number: ")
 
 
 # Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the
@@ -34,3 +51,19 @@
 # C
 # Enter score: 0.5
 # F
+try:
+    score = float(input("Enter score: "))
+    if score >=0.9:
+        print("A")
+    elif 0.90 > score >= 0.8:
+        print("B")
+    elif 0.9 > score >= 0.8:
+        print("B")
+    elif 0.8 > score >= 0.7:
+        print("C")
+    elif 0.7 > score >= 0.6:
+        print("D")
+    else:
+        print("F")
+except:
+    print("Bad score")
