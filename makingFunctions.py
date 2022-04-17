8a002a28c6c2ca9b607dca011ccd756d75660671
Lsobaca <61958964+Lsobaca@users.noreply.github.com>
2022-04-17 16:40:48 -0700

    started to add some stuff in the file explaining what are some functions

---
+++ makingFunctions.py
@@ -0,0 +1,17 @@
+# what is a function?
+# a function is a sequence of statements
+# that does computation. we can call on them
+# later or reuse them many more times. sometimes
+# they can have arguments or not take in anything
+
+# some functions are already built in such as the math
+# module
+import math
+# would print "w" since w has the highest value
+max("hello word")
+
+# type casting is a built in function that would convert 
+# the value to the disired value
+# this would return '32' 
+
+int('32')+1
